7730cdebdde1a0909970ea9ee66ca234a9482a55
Janna Wennberg <janna.wennberg@gmail.com>
2026-05-15 16:56:58 -0700

    added VVIQ

---
+++ jspsych_lib/questionnaires.js
@@ -0,0 +1,614 @@
+var irq = [
+  {
+    "item_id": "irq-manipulation-1",
+    "item": "I can easily imagine and mentally rotate three-dimensional geometric figures.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-manipulation-2",
+    "item": "I can easily choose to imagine this sentence in my mind pronounced unnaturally slowly.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-manipulation-3",
+    "item": "In school, I had no problems with geometry.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-manipulation-4",
+    "item": "It is easy for me to imagine the sensation of licking a brick.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-manipulation-5",
+    "item": "I find it difficult to imagine how a three-dimensional geometric figure would exactly look like when rotated.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-manipulation-6",
+    "item": "I can easily imagine someone clearly talking, and then imagine the same voice with a heavy cold.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-manipulation-7",
+    "item": "I think I have a large vocabulary in my native language compared to others.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-manipulation-8",
+    "item": "I can easily imagine the sound of a trumpet getting louder.",
+    "category": "manipulation",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-ortho-1",
+    "item": "I find it easy to decide if words rhyme by seeing their spelling in my mind's eye.",
+    "category": "orthographic",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-ortho-2",
+    "item": "When I hear someone talking, I see words written down in my mind.",
+    "category": "orthographic",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-ortho-3",
+    "item": "I see words in my \"mind’s eye\" when I think.",
+    "category": "orthographic",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-ortho-4",
+    "item": "When I am introduced to someone for the first time, I imagine what their name would look like when written down.",
+    "category": "orthographic",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-ortho-5",
+    "item": "A strategy I use to help me remember written material is imagining what the writing looks like.",
+    "category": "orthographic",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-ortho-6",
+    "item": "I hear a running summary of everything I am doing in my head.",
+    "category": "orthographic",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-ortho-7",
+    "item": "I rehearse in my mind how someone might respond to a text message before I send it.",
+    "category": "orthographic",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-1",
+    "item": "When I think about someone I know well, I instantly hear their voice in my mind.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-2",
+    "item": "I think about problems in my mind in the form of a conversation with myself.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-3",
+    "item": "If I am walking somewhere by myself, I often have a silent conversation with myself.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-4",
+    "item": "If I am walking somewhere by myself, I frequently think of conversations that I’ve recently had.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-5",
+    "item": "My inner speech helps my imagination.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-6",
+    "item": "I tend to think things through verbally when I am relaxing.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-7",
+    "item": "When thinking about a social problem, I often talk it through in my head.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-8",
+    "item": "I like to give myself some down time to talk through thoughts in my mind.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-9",
+    "item": "I hear words in my \"mind’s ear\" when I think.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-10",
+    "item": "I rarely vocalize thoughts in my mind.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-11",
+    "item": "I often talk to myself internally while watching TV.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-verbal-12",
+    "item": "My memories often involve conversations I’ve had.",
+    "category": "verbal",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-1",
+    "item": "I often enjoy the use of mental pictures to reminisce.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-2",
+    "item": "I can close my eyes and easily picture a scene that I have experienced.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-3",
+    "item": "My mental images are very vivid and photographic.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-4",
+    "item": "The old saying “A picture is worth a thousand words” is certainly true for me.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-5",
+    "item": "When I think about someone I know well, I instantly see their face in my mind.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-6",
+    "item": "I often use mental images or pictures to help me remember things.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-7",
+    "item": "My memories are mainly visual in nature.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-8",
+    "item": "When traveling to get to somewhere I tend to think more visually than verbally.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-9",
+    "item": "If I talk to myself in my head it is usually accompanied by visual imagery.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-visual-10",
+    "item": "If I imagine my memories visually they are more often static than moving.",
+    "category": "visual",
+    "questionnaire": "irq"
+  },
+  {
+    "item_id": "irq-catch-1",
+    "item": "The word ‘hotel’ has three letters.",
+    "category": "catch",
+    "questionnaire": "irq"
+  }
+]
+
+var osivq = [
+  {
+    "item_id": "osviq-object-1",
+    "item": "My mental images are very colourful and bright.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-2",
+    "item": "When reading fiction, I usually form a clear and detailed mental picture of a scene or room that has been described.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-3",
+    "item": "If I were asked to choose among engineering professions, or visual arts, I would choose visual arts.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-4",
+    "item": "I have a photographic memory.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-5",
+    "item": "I enjoy pictures with bright colours and unusual shapes like the ones in modern art.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-6",
+    "item": "When entering a familiar store to get a specific item, I can easily picture the exact location of the target item, the shelf it stands on, how it is arranged and the surrounding articles.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-7",
+    "item": "My mental images are very vivid and photographic.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-8",
+    "item": "My mental images of different objects very much resemble the size, shape and colour of actual objects that I have seen.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-9",
+    "item": "When I imagine the face of a friend, I have a perfectly clear and bright image.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-10",
+    "item": "I can easily remember a great deal of visual details that someone else might never notice. For example, I would just automatically take some things in, like what colour is a shirt someone wears or what colour are his/her shoes.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-11",
+    "item": "Sometimes my mental images are so vivid and persistent that it is difficult to ignore them.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-12",
+    "item": "I can close my eyes and easily picture a scene that I have experienced.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-13",
+    "item": "I remember everything visually. I can recount what people wore to a dinner and I can talk about the way they sat and the way they looked probably in more detail than I could discuss what they said.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-14",
+    "item": "My visual images are in my head all the time. They are just right there.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-object-15",
+    "item": "When I hear a radio announcer or a DJ I’ve never actually seen, I usually find myself picturing what he or she might look like.",
+    "category": "visual",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-1",
+    "item": "I was very good in 3D geometry as a student.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-2",
+    "item": "If I were asked to choose between engineering professions and visual arts, I would prefer engineering.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-3",
+    "item": "Architecture interests me more than painting.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-4",
+    "item": "I prefer schematic diagrams and sketches when reading a textbook instead of colourful and pictorial illustrations.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-5",
+    "item": "My mental images are more like schematic representations of things and events rather than like detailed pictures.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-6",
+    "item": "I can easily imagine and mentally rotate three-dimensional geometric figures.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-7",
+    "item": "When thinking about an abstract concept (or building), I imagine an abstract schematic building in my mind or its blueprint rather than a specific concrete building.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-8",
+    "item": "I normally do not experience many spontaneous vivid images; I use my mental imagery mostly when attempting to solve some problems like the ones in mathematics.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-9",
+    "item": "I have excellent abilities in technical graphics.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-10",
+    "item": "I can easily sketch a blueprint for a building I am familiar with.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-11",
+    "item": "In school, I had no problems with geometry.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-12",
+    "item": "I am good in playing spatial games involving constructing from blocks and paper (e.g. Lego, Tetris, Origami).",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-13",
+    "item": "My mental images are more schematic than colourful and pictorial.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-14",
+    "item": "I find it difficult to imagine how a three-dimensional geometric figure would exactly look like when rotated.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-spatial-15",
+    "item": "My graphic abilities would make a career in architecture relatively easy for me.",
+    "category": "spatial",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-1",
+    "item": "I have difficulty expressing myself in writing.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-2",
+    "item": "My verbal abilities would make a career in language arts relatively easy for me.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-3",
+    "item": "I tell jokes and stories better than most people.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-4",
+    "item": "Essay writing is difficult for me and I do not enjoy doing it at all.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-5",
+    "item": "My verbal skills are excellent.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-6",
+    "item": "Putting together furniture kits (e.g. a TV stand or a chair)is much easier for me when I have detailed verbal instructions than when I only have a diagram or picture.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-7",
+    "item": "When explaining something, I would rather give verbal explanations than make drawings or sketches.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-8",
+    "item": "If someone were to give me two-digit numbers to add (e.g. 43 and 32) I would simply do the adding without visualizing the numbers.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-9",
+    "item": "I usually do not try to visualize or sketch diagrams when reading a textbook.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-10",
+    "item": "When remembering a scene, I use verbal descriptions rather than mental pictures.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-11",
+    "item": "I have better than average fluency in using words.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-12",
+    "item": "I would rather have a verbal description of an object or person than a picture.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-13",
+    "item": "I am always aware of sentence structure.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-14",
+    "item": "I enjoy being able to rephrase my thoughts in many ways for variety’s sake in both writing and speaking.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osviq-verbal-15",
+    "item": "I sometimes have a problem expressing exactly what I want to say.",
+    "category": "verbal",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osivq-catch-1",
+    "item": "Elephants are larger than dogs.",
+    "category": "catch",
+    "questionnaire": "osivq"
+  },
+  {
+    "item_id": "osivq-catch-2",
+    "item": "Most people have five legs.",
+    "category": "catch",
+    "questionnaire": "osivq"
+  }
+]
+
+var vviq = [
+  {
+    "item_id": "vviq-1",
+    "item": "The exact contour of face, head, shoulders, and body",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-2",
+    "item": "Characteristic poses of head, attitudes of body, etc.",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-3",
+    "item": "The precise carriage, length of step, etc. in walking",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-4",
+    "item": "The different colors worn in some familiar clothes",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-5",
+    "item": "The sun is rising above the horizon into a hazy sky.",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-6",
+    "item": "The sky clears and surrounds the sun with blueness.",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-7",
+    "item": "Clouds. A storm blows up, with flashes of lightening.",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-8",
+    "item": "A rainbow appears.",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-9",
+    "item": "The overall appearance of the shop from the opposite side of the road",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-10",
+    "item": "A window display including colors, shape, and details of individual items for sale",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-11",
+    "item": "You are near the entrance. The color, shape, and details of the door",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-12",
+    "item": "You enter the shop and go to the counter. The counter assistant serves you. Money changes hands.",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-13",
+    "item": "The contours of the landscape",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-14",
+    "item": "The color and shape of the trees",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-15",
+    "item": "The color and shape of the lake",
+    "category": "visual",
+    "questionnaire": "vviq"
+  },
+  {
+    "item_id": "vviq-16",
+    "item": "A strong wind blows on the tree and on the lake causing waves.",
+    "category": "visual",
+    "questionnaire": "vviq"
+  }
+]
